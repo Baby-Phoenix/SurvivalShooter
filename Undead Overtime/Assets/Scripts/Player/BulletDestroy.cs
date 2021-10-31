dced91fd2056eaeda1e724e31600f4fdb899040a
Mrinank <mrinank.sivakumar@ontariotechu.net>
2021-10-31 00:46:38 -0400

    Added URP version of project

---
+++ Undead Overtime/Assets/Scripts/Player/BulletDestroy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy (gameObject, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
